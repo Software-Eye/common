d9040fc9f05dfa43550e69193ffd3d957912bde5
Roeei itshayek <roeeiit1@gmail.com>
2023-01-08 10:03:16 +0200

    Added support for pojos in mongo

--- src/main/java/org/softwareeyes/common/clients/SoftwareEyesMongoClient.java
+++ src/main/java/org/softwareeyes/common/clients/SoftwareEyesMongoClient.java
@@ -3,36 +3,30 @@ package org.softwareeyes.common.clients;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mongodb.MongoClient;
+import com.mongodb.MongoClientOptions;
+import com.mongodb.MongoClientSettings;
 import com.mongodb.MongoClientURI;
 import com.mongodb.client.MongoCollection;
 import org.bson.Document;
+import org.bson.codecs.configuration.CodecRegistry;
+import org.bson.codecs.pojo.PojoCodecProvider;
 
 import java.util.Map;
 
+import static org.bson.codecs.configuration.CodecRegistries.fromProviders;
+import static org.bson.codecs.configuration.CodecRegistries.fromRegistries;
+
 public class SoftwareEyesMongoClient implements AutoCloseable {
-    private MongoClient mongoClient;
-    private String databaseName;
+    private final MongoClient mongoClient;
+    private final String databaseName;
     private String collectionName ;
 
     public SoftwareEyesMongoClient(String connectionString, String databaseName, String collectionName){
-        MongoClientURI uri = new MongoClientURI(connectionString);
+        MongoClientOptions.Builder clientOptions = MongoClientOptions.builder().codecRegistry(buildPojoCodecRegistry());
+        MongoClientURI uri = new MongoClientURI(connectionString, clientOptions);
         this.databaseName = databaseName;
         this.collectionName = collectionName;
         this.mongoClient = new MongoClient(uri);
-    }
-
-    public MongoCollection<Document> getQuerier(){
-        return mongoClient.getDatabase(databaseName).getCollection(collectionName);
-    }
-
-    public static Document convertObjectToDocument(Object obj){
-        ObjectMapper objectMapper = new ObjectMapper();
-        Map map = objectMapper.convertValue(obj, Map.class);
-        return new Document(map);
-    }
-    @Override
-    public void close() throws Exception {
-        mongoClient.close();
     }
 
     public String getCollectionName() {
@@ -41,5 +35,28 @@ public class SoftwareEyesMongoClient implements AutoCloseable {
 
     public void setCollectionName(String collectionName) {
         this.collectionName = collectionName;
+    }
+
+    public <TDocument> MongoCollection<TDocument> getQuerier(Class<TDocument> documentClass){
+        return mongoClient.getDatabase(databaseName).getCollection(collectionName, documentClass);
+    }
+
+    public MongoCollection<Document> getQuerier(){
+        return mongoClient.getDatabase(databaseName).getCollection(collectionName);
+    }
+
+    public static Document convertObjectToDocument(Object objectToConvert){
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map objectMap = objectMapper.convertValue(objectToConvert, Map.class);
+        return new Document(objectMap);
+    }
+    @Override
+    public void close() throws Exception {
+        mongoClient.close();
+    }
+
+    private CodecRegistry buildPojoCodecRegistry(){
+        return fromRegistries(MongoClientSettings.getDefaultCodecRegistry(),
+                fromProviders(PojoCodecProvider.builder().register("org.softwareeyes.common").automatic(true).build()));
     }
 }
