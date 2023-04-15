680fe5f2b7ebcd697dea7b86c5a6dd12eaa80e09
Roeei itshayek <roeeiit1@gmail.com>
2023-04-15 19:49:25 +0300

    Added Constructor to combinedExporter and moved to new Objects

--- src/main/java/org/softwareeyes/common/models/CombinedExporter.java
+++ src/main/java/org/softwareeyes/common/models/CombinedExporter.java
@@ -1,13 +1,23 @@
 package org.softwareeyes.common.models;
 
+import org.softwareeyes.common.models.exporterConfiguration.ExporterConfigurationParametersBase;
+import org.softwareeyes.common.models.exporterConfiguration.ExporterMetadata;
+import org.softwareeyes.common.models.exporterConfiguration.exporterTopology.ExporterTopology;
+
 import java.util.ArrayList;
 
-public class CombinedExporter extends Exporter {
+public class CombinedExporter extends org.softwareeyes.common.models.exporterConfiguration.ExporterConfiguration {
     ArrayList<Rule> rules;
 
-    public CombinedExporter(ExporterMetaData metaData, ExporterConfiguration configuration, Topology topology, ArrayList<Rule> rules) {
-        super(metaData, configuration, topology);
+    public CombinedExporter(ExporterMetadata metadata, ExporterConfigurationParametersBase exporterConfigurationParameters, ExporterTopology topology, String id, ArrayList<Rule> rules) {
+        super(metadata, exporterConfigurationParameters, topology, id);
         this.rules = rules;
+    }
+
+    public CombinedExporter(CombinedExporter combinedExporter){
+        super(combinedExporter.getMetadata(), combinedExporter.getExporterConfigurationParameters(),
+                combinedExporter.getTopology(), combinedExporter.getId());
+        this.rules = combinedExporter.rules;
     }
 
     public ArrayList<Rule> getRules() {
